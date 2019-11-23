6dc85e5cfece7445d79e573d4c25b7af8fcf54e9
Gia Edgington <gia.edgington@gmail.com>
2019-11-23 14:34:04 -0500

    image form header/ css added

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Florist from './Florist';
+import flowers from '../images/flowers.png';
 
 class Form extends React.Component {
 
@@ -7,13 +8,13 @@ class Form extends React.Component {
         location: '',
         florists: []
     }
-
+    //Get user's input
     handleChange = (event) => {
         this.setState({
             location: event.target.value
         })
     }
-
+    //Fetch backend with location provided
     handleSubmit = (event) => {
         event.preventDefault();
         var location = this.state.location;
@@ -30,15 +31,16 @@ class Form extends React.Component {
             return <Florist key={index} name={florist.name} phone={florist.display_phone} address={florist.location.display_address[0]+ florist.location.display_address[1]} />
         })
         return(
-            <div className="">
-                <div className="">
+            <div className="form-header">
+                <img src = {flowers} alt=''/>
+                <div className="searchForm">
                     <form onSubmit={this.handleSubmit}>
                             <label>Find Local Florists</label><br/>
                             <input type="text" autoComplete="off" id="location" placeholder="address, neighborhood, city, state or zip" onChange={this.handleChange}></input><br/>
                             <button>Search</button>
                     </form>
             </div>
-                <div className="">
+                <div className="floristList">
                     <div>
                     {localFlorists}
                     </div>
